0476e7e4f87902e2455944c9b22437e848b36c03
morghan <joel.hz24@gmail.com>
2022-02-21 15:21:57 -0600

    info site structure

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+	return <h1>Main goes here</h1>;
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Navbar() {
+	return <h1>Navbar goes here</h1>;
+}
+
+export default Navbar;
