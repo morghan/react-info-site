aae3b37bdf1354fd3306cf8b0ec677829eef5049
morghan <joel.hz24@gmail.com>
2022-02-22 14:24:35 -0600

    Main section styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function Main() {
-	return <h1>Main goes here</h1>;
+	return (
+		<div className='main'>
+			<h1 className='mainHeader'>Fun facts about React</h1>
+			<ul className='mainText'>
+				<li>Was first released in 2013</li>
+				<li>Was originally created by Jordan Walke</li>
+				<li>Has well over 100K stars on GitHub</li>
+				<li>Is maintained by Facebook</li>
+				<li>Powers thousands of enterprise apps, including mobile apps</li>
+			</ul>
+		</div>
+	);
 }
 
 export default Main;
